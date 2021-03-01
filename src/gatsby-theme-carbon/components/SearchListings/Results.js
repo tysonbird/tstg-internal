05c62295123ed3ac73ad223aa3d8c9bdc69b9f1b
Tyson Bird <tbird@L-C02X6113JGH5.local>
2021-03-01 17:07:02 -0600

    Added ads to region pages. Small bug fixes.

--- src/gatsby-theme-carbon/components/SearchListings/Results.js
+++ src/gatsby-theme-carbon/components/SearchListings/Results.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { results, resultsbox } from '../ComponentStyles/SearchResults.module.scss';
 import { Launch32 } from '@carbon/icons-react';
-
+import AdGroup from '../Ad/AdGroup.js'
 
 // function titleCase(str) {
 //     return str.toLowerCase().split(' ').map(function(word) {
@@ -11,18 +11,25 @@ import { Launch32 } from '@carbon/icons-react';
 
 class Results extends Component {
   render() {
-    const options = this.props.listings.map(r => (
-      r["Description for TravelTex.com"] && 
-        <div key={r["AttractionId"]} className={resultsbox}>
-        <h3>{r["Attraction Title"]}</h3>
-        <p><span>{r["City"]}</span>
-        {r["Description for TravelTex.com"]}</p>
-        {r["Hours"] && <p><span>Hours: </span>{r["Hours"]}</p>}
-        {r["Address"] && <p><span>Address: </span>{r["Address"]}</p>}
-        {r["Website"] && <p><a rel="noopener noreferrer" target="_blank" href={'http://' + r["Website"]}>Website</a> <i><Launch32/></i></p>}
-        {r["Phone"] && <p><span>Phone: </span>{r["Phone"]}</p>}
-      </div>
-    ))
+    const options = this.props.listings.map((r, index) => {
+      if (this.props.ads !== undefined && index % 7 === 0 && index !== 0) {
+        return <AdGroup id={this.props.ads} key={index}/>
+      }
+      else {
+        return(
+          r["Description for TravelTex.com"] && 
+            <div key={r["AttractionId"]} className={resultsbox}>
+            <h3>{r["Attraction Title"]}</h3>
+            <p><span>{r["City"]}</span>
+            {r["Description for TravelTex.com"]}</p>
+            {r["Hours"] && <p><span>Hours: </span>{r["Hours"]}</p>}
+            {r["Address"] && <p><span>Address: </span>{r["Address"]}</p>}
+            {r["Website"] && <p><a rel="noopener noreferrer" target="_blank" href={'http://' + r["Website"]}>Website</a> <i><Launch32/></i></p>}
+            {r["Phone"] && <p><span>Phone: </span>{r["Phone"]}</p>}
+          </div>
+        )
+      }
+  })
     return (
       <div className={results}>{options}</div>
     )
