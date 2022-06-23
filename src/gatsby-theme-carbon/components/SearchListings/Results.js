2c6baf4bf9cf59c4ffe8cb95397c30518161ee9e
Tyson Bird <tbird@L-C02X6113JGH5.local>
2022-06-23 11:38:45 -0500

    Updated results list to not ignore seventh listing

--- src/gatsby-theme-carbon/components/SearchListings/Results.js
+++ src/gatsby-theme-carbon/components/SearchListings/Results.js
@@ -13,7 +13,20 @@ class Results extends Component {
   render() {
     const options = this.props.listings.map((r, index) => {
       if (this.props.ads !== undefined && index % 7 === 0 && index !== 0) {
-        return <AdGroup id={this.props.ads} key={index}/>
+        return (
+          <>
+          <AdGroup id={this.props.ads} key={index}/>
+          <div key={r["AttractionId"]} className={resultsbox}>
+            <h3>{r["Attraction Title"]}</h3>
+            <p><span>{r["City"]}</span>
+            {r["Description for TravelTex.com"]}</p>
+            {r["Hours"] && <p><span>Hours: </span>{r["Hours"]}</p>}
+            {r["Address"] && <p><span>Address: </span>{r["Address"]}</p>}
+            {r["Website"] && <p><a rel="noopener noreferrer" target="_blank" href={'http://' + r["Website"]}>Website</a> <i><Launch32/></i></p>}
+            {r["Phone"] && <p><span>Phone: </span>{r["Phone"]}</p>}
+          </div>
+          </>
+        )
       }
       else {
         return(
